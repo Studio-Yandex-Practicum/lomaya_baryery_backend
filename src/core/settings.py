7684e92b6f7e9c2915fcd995e205a76fa920c8a4
sekachev <sekachev@proactor.pro>
2022-09-13 22:35:35 +0300

    postgresql+asyncpg

--- src/core/settings.py
+++ src/core/settings.py
@@ -17,9 +17,7 @@ POSTGRES_PASSWORD = env("POSTGRES_PASSWORD")  # пароль для подклю
 DB_HOST = env("DB_HOST")  # название сервиса (контейнера)
 DB_PORT = env("DB_PORT")  # порт для подключения к БД
 
-# DATABASE_URL = (
-#     "postgresql+asyncpg://"
-#     f"{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# )
-
-DATABASE_URL = "sqlite+aiosqlite:///./sql_db.db"
+DATABASE_URL = (
+    "postgresql+asyncpg://"
+    f"{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
