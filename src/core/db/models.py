f5322c3980cce6b4c15972c3e78c7fda567f3e08
sekachev <sekachev@proactor.pro>
2022-09-14 11:36:23 +0300

    uuid_pkg

--- src/core/db/models.py
+++ src/core/db/models.py
@@ -1,3 +1,4 @@
+import uuid as uuid_pkg
 from datetime import datetime
 
 from sqlmodel import SQLModel, Field
@@ -5,7 +6,12 @@ from sqlmodel import SQLModel, Field
 
 class BaseModel(SQLModel):
     """Базовая модель."""
-    id: int = Field(primary_key=True, nullable=False)
+    id: uuid_pkg.UUID = Field(
+        default_factory=uuid_pkg.uuid4,
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
     updated_at: datetime = Field(default=datetime.utcnow())
     created_at: datetime = Field(default=datetime.utcnow())
 
