9ed40e21dc0ff662ef309b82839a7c022ed153a0
sekachev <sekachev@proactor.pro>
2022-09-17 17:59:48 +0300

    refactoring

--- src/core/db/db.py
+++ src/core/db/db.py
@@ -5,6 +5,7 @@ from src.core.settings import DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 
+
 async def get_session() -> AsyncSession:
     async_session = sessionmaker(
         engine, class_=AsyncSession, expire_on_commit=False
