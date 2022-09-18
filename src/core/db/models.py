8c3945f1c4617257b80a5e3ae409f0b750e54741
sniki-ld <dennisalenaf@gmail.com>
2022-09-18 18:28:20 +0300

    add deleted parameter to Base model

--- src/core/db/models.py
+++ src/core/db/models.py
@@ -1,7 +1,7 @@
 import uuid
 
-from sqlalchemy import func, Column, TIMESTAMP, DATE, String, types
-from sqlalchemy.dialects.postgresql import UUID, ENUM
+from sqlalchemy import DATE, TIMESTAMP, Boolean, Column, String, func, types
+from sqlalchemy.dialects.postgresql import ENUM, UUID
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
@@ -11,6 +11,7 @@ class Base:
     id = Column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
+    deleted = Column(Boolean, default=0)
     created_date = Column(
         TIMESTAMP, server_default=func.current_timestamp(), nullable=False
     )
