15d847685a2f87292e405e4c7dc54aed816b80ed
Bogdan Stefanyuk <86630295+Ponimon4ik@users.noreply.github.com>
2022-09-18 18:58:32 +0300

    changed parameters of functions, moved constants to body of functions, delete class

--- src/bot/services.py
+++ src/bot/services.py
@@ -1,41 +1,28 @@
-from telegram import Bot
-
-from src.main import create_bot
 from src.core import settings
+from src.core.db import models
+from src.main import create_bot
 
-
-APPROVAL_MESSAGE = (
-    'Привет, {first_name} {second_name} пользователя!'
-    'Поздравляем, ты в проекте!'
-)
-REJECTION_MESSAGE = (
-    f'К сожалению, на данный момент мы не можем зарегистрировать вас в '
-    f'проекте. Вы можете написать на почту {settings.ORGANIZATIONS_EMAIL}. Чтобы не '
-    'пропустить актуальные новости Центра "Ломая барьеры" - вступайте '
-    f'в нашу группу {settings.ORGANIZATIONS_GROUP}'
-)
 
 bot = create_bot().bot  # временная копия бота до миграции на webhooks
 
 
-class VerificationNotification:
-    """Класс для отправки уведомления пользователю
-    о результате проверки его заявки на участие в проекте"""
-
-    def __init__(self, bot: Bot):
-        self.bot = bot
-
-    async def send_approval_callback(self, id, first_name, second_name):
-        await self.bot.send_message(
-            chat_id=id,
-            text=APPROVAL_MESSAGE.format(
-                first_name=first_name,
-                second_name=second_name
-            )
+async def send_approval_callback(user: models.User):
+    await bot.send_message(
+        chat_id=user.telegram_id,
+        text=(
+            f'Привет, {user.name} {user.surname}! Поздравляем, ты в проекте!'
         )
+    )
 
-    async def send_rejection_callback(self, id):
-        await self.bot.send_message(
-            chat_id=id,
-            text=REJECTION_MESSAGE
+
+async def send_rejection_callback(user: models.User):
+    await bot.send_message(
+        chat_id=user.telegram_id,
+        text=(
+            f'К сожалению, на данный момент мы не можем зарегистрировать вас'
+            f' в проекте. Вы можете написать на почту '
+            f'{settings.ORGANIZATIONS_EMAIL}. Чтобы не пропустить актуальные'
+            f' новости Центра "Ломая барьеры" - вступайте в нашу группу '
+            f'{settings.ORGANIZATIONS_GROUP}'
         )
+    )
