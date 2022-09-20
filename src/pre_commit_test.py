a525ea60e2342ec50171c8fd12a96c3e0fb27571
AlxShvalev <shvalevalx@gmail.com>
2022-09-20 21:46:07 +0500

    test

--- src/pre_commit_test.py
+++ src/pre_commit_test.py
@@ -5,5 +5,6 @@ from api.routers import router
 router.dependency_overrides_provider('sadfkjbkbsadf.nbsdf.,mbas,mabf.,amsbdf.m,bas.mdngb.ams')
 
 now = datetime.datetime.now()
-
+text = 'asdjkbhdfkjbsdfkn'
 print(now)
+print(text)
