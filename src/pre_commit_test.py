bebc37ab2603bf163b4039ba856c8177353d0c6b
AlxShvalev <shvalevalx@gmail.com>
2022-09-20 22:07:55 +0500

    test

--- src/pre_commit_test.py
+++ src/pre_commit_test.py
@@ -2,10 +2,13 @@ import datetime
 
 from api.routers import router
 
-router.dependency_overrides_provider('sadfkjbkbsadf.nbsdf.,mbas,mabf.,amsbdf.m,bas.mdngb.ams')
+router.dependency_overrides_provider(
+    "sadfkjbkbsadf.nbsdf.,mbas,mabf.,amsbdf.m,bas.mdngb.ams"
+)
 
 now = datetime.datetime.now()
-text = 'asdjkbhdfkjbsdfkn'
-text1 = 'asfjhljhasfvlj'
+text = "asdjkbhdfkjbsdfkn"
+text1 = "asfjhljhasfvlj"
+
 print(now)
 print(text)
