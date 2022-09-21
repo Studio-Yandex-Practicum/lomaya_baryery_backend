acc16b85f29fe7fd7bd9301a0dbc16e58169cbaf
Ivan Skvortsov <pprofcheg@gmail.com>
2022-09-21 11:54:54 +0300

    restored run_bot.py

--- run_bot.py
+++ run_bot.py
@@ -1,9 +1,4 @@
-from src.main import create_bot, get_bot_webhook_settings
-from src.core.settings import settings
+from src.main import create_bot
 
 if __name__ == '__main__':
-    if settings.BOT_WEBHOOK_MODE:
-        webhook_settings = get_bot_webhook_settings()
-        create_bot().run_webhook(**webhook_settings)
-    else:
-        create_bot().run_polling()
+    create_bot().run_polling()
