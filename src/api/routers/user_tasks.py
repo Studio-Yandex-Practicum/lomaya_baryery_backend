b0789f074cd3d656e1b329bbf2ec929e314a3b1c
sniki-ld <dennisalenaf@gmail.com>
2022-09-25 12:53:01 +0300

    fix user_tasks.py

--- src/api/routers/user_tasks.py
+++ src/api/routers/user_tasks.py
@@ -1,10 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException
 from pydantic.schema import UUID
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.response_models.user_task import UserTaskDB
-from src.api.routers.validators import check_user_task_exists
-from src.core.db.db import get_session
 from src.core.services.user_task_service import UserTaskService
 
 router = APIRouter()
@@ -15,8 +12,9 @@ router = APIRouter()
     response_model=UserTaskDB,
     response_model_exclude_none=True,
 )
-async def get_users_report(user_task_id: UUID, session: AsyncSession = Depends(get_session)):
+async def get_users_report(user_task_id: UUID):
     """Вернуть отчет участника."""
-    user_task = await UserTaskService.get(user_task_id=user_task_id, session=session)
-    await check_user_task_exists(user_task)
+    user_task = await UserTaskService.get(user_task_id=user_task_id)
+    if user_task is None:
+        raise HTTPException(status_code=404, detail="Задачи с указанным id не существует!")
     return user_task
--- src/core/services/user_task_service.py
+++ src/core/services/user_task_service.py
@@ -1,18 +1,19 @@
 from pydantic.schema import UUID
 from sqlalchemy import select
-from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.core.db.models import Photo, UserTask
 
 
 class UserTaskService:
+    def __init__(self, session):
+        self.session = session
+
     async def get(
         self,
         user_task_id: UUID,
-        session: AsyncSession,
     ):
         """Получить объект отчета участника по id."""
-        user_task = await session.execute(
+        user_task = await self.session.execute(
             select(UserTask, Photo.url.label("photo_url")).where(UserTask.id == user_task_id)
         )
         return user_task.scalars().first()
