94691de789886321e6be82c1c110ce18d2f350cf
sniki-ld <dennisalenaf@gmail.com>
2022-09-26 18:14:12 +0300

    fix: add Dependency Injection

--- src/api/response_models/user_task.py
+++ src/api/response_models/user_task.py
@@ -1,15 +1,16 @@
 from pydantic import BaseModel
 from pydantic.schema import UUID
 
-from src.core.db.models import UserTaskStatuses
+from src.core.db.models import UserTask
 
 
 class UserTaskDB(BaseModel):
-    """Pydantic-схема, для описания объекта, полученного из БД"""
+    """Pydantic-схема, для описания объекта, полученного из БД."""
+
     user_task_id: UUID
     task_id: UUID
     day_number: int
-    status: UserTaskStatuses
+    status: UserTask.Status
     photo_url: str
 
     class Config:
--- src/api/routers/user_tasks.py
+++ src/api/routers/user_tasks.py
@@ -1,22 +1,19 @@
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic.schema import UUID
-from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.api.response_models.user_task import UserTaskDB
-from src.core.db.db import get_session
-from src.core.services.user_task_service import UserTaskService
+from src.core.services.user_task_service import UserTaskService, get_user_task_service
 
 router = APIRouter()
 
 
 @router.get(
-    "/{user_task_id}",
+    "/user/task/{user_task_id}",
     response_model=UserTaskDB,
     response_model_exclude_none=True,
 )
-async def get_users_report(user_task_id: UUID, session: AsyncSession = Depends(get_session)):
+async def get_users_report(user_task_id: UUID, user_task_service: UserTaskService = Depends(get_user_task_service)):
     """Вернуть отчет участника."""
-    user_task_service = UserTaskService(session=session)
     user_task = await user_task_service.get(user_task_id=user_task_id)
     if user_task is None:
         raise HTTPException(status_code=404, detail="Задачи с указанным id не существует!")
