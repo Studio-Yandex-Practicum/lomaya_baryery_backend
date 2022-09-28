3b5f439c276e32d898d8977a6b26a432f6c74ed7
Ivan Skvortsov <pprofcheg@gmail.com>
2022-09-28 19:57:21 +0300

    fixed misspell in PhotoRepository class name

--- src/core/db/repository/photo_repository.py
+++ src/core/db/repository/photo_repository.py
@@ -7,7 +7,7 @@ from src.core.db.models import Photo
 from src.core.db.repository import AbstractRepository
 
 
-class PhotoRepositopry(AbstractRepository):
+class PhotoRepository(AbstractRepository):
     """Репозиторий для работы с моделью Photo."""
 
     def __init__(self, session: AsyncSession):
