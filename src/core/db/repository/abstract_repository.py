573692b063705287020104316e46a54cd57a9833
Ivan Skvortsov <pprofcheg@gmail.com>
2022-09-28 15:41:37 +0300

    first implementation of abstract repository class

---
+++ src/core/db/repository/abstract_repository.py
@@ -0,0 +1,41 @@
+import abc
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel as PydanticModel
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.db.models import Base as DatabaseModel
+
+
+class AbstractRepository(abc.ABC):
+    """Абстрактный класс, реализующий паттерн Repository."""
+
+    @abc.abstractmethod
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        self.model = DatabaseModel
+
+    @abc.abstractmethod
+    async def get(self, obj_id: DatabaseModel.id) -> DatabaseModel:
+        return await self.session.scalar(select(self.model).where(self.model.id == obj_id))
+
+    @abc.abstractmethod
+    async def create(self, obj_data: PydanticModel) -> DatabaseModel:
+        obj_data = obj_data.dict()
+        db_obj = self.model(**obj_data)
+        await self.session.commit()
+        await self.session.refresh(db_obj)
+        return db_obj
+
+    @abc.abstractmethod
+    async def update(self, db_obj: DatabaseModel, obj_update_data: PydanticModel) -> DatabaseModel:
+        obj_current_data = jsonable_encoder(db_obj)
+        obj_update_data = obj_update_data.dict(exclude_unset=True)
+        for field in obj_current_data:
+            for field in obj_update_data:
+                setattr(db_obj, field, obj_update_data[field])
+        self.session.add(db_obj)
+        self.session.commit()
+        self.session.refresh(db_obj)
+        return db_obj
