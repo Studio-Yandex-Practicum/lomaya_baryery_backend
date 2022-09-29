16887651cc97e7e3c95cdaa38fdf9fe9c8da650f
Ivan Skvortsov <pprofcheg@gmail.com>
2022-09-29 14:14:58 +0300

    reworked UserRepository

--- src/core/db/repository/user_repository.py
+++ src/core/db/repository/user_repository.py
@@ -1,8 +1,10 @@
+from typing import Optional
 from uuid import UUID
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api.request_models.user import UserRequest
+from src.core.db.db import get_session
 from src.core.db.models import User
 from src.core.db.repository import AbstractRepository
 
@@ -10,15 +12,27 @@ from src.core.db.repository import AbstractRepository
 class UserRepository(AbstractRepository):
     """Репозиторий для работы с моделью User."""
 
-    def __init__(self, session: AsyncSession):
-        super().__init__(session)
-        self.model = User
+    def __init__(self, session: AsyncSession = Depends(get_session)) -> None:
+        self.session = session
 
-    async def get(self, obj_id: UUID) -> User:
-        return await super().get(obj_id)
+    async def get_or_none(self, id: UUID) -> Optional[User]:
+        return await self.session.get(User, id)
 
-    async def create(self, obj_data: UserRequest) -> User:
-        return await super().create(obj_data)
+    async def get(self, id: UUID) -> User:
+        user = await self.get_or_none(id)
+        if user is None:
+            # FIXME: написать и использовать кастомное исключение
+            raise LookupError(f"Объект User c {id=} не найден.")
+        return user
 
-    async def update(self):
-        raise NotImplementedError(f"Метод `update` не определен в репозитории {self.__class__.__name__}")
+    async def create(self, user: User) -> User:
+        self.session.add(user)
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user
+
+    async def update(self, id: UUID, user: User) -> User:
+        user.id = id
+        await self.session.merge(user)
+        await self.session.commit()
+        return user
