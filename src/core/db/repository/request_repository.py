ac1bdb8003020de24da1a8ba75377824b5dd3fd8
Ivan Skvortsov <pprofcheg@gmail.com>
2022-09-29 14:12:57 +0300

    reworked RequestRepository

--- src/core/db/repository/request_repository.py
+++ src/core/db/repository/request_repository.py
@@ -1,8 +1,10 @@
+from typing import Optional
 from uuid import UUID
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api.request_models.request import RequestRequest
+from src.core.db.db import get_session
 from src.core.db.models import Request
 from src.core.db.repository import AbstractRepository
 
@@ -10,15 +12,27 @@ from src.core.db.repository import AbstractRepository
 class RequestRepository(AbstractRepository):
     """Репозиторий для работы с моделью Request."""
 
-    def __init__(self, session: AsyncSession):
-        super().__init__(session)
-        self.model = Request
+    def __init__(self, session: AsyncSession = Depends(get_session)) -> None:
+        self.session = session
 
-    async def get(self, obj_id: UUID) -> Request:
-        return await super().get(obj_id)
+    async def get_or_none(self, id: UUID) -> Optional[Request]:
+        return await self.session.get(Request, id)
 
-    async def create(self, obj_data: RequestRequest) -> Request:
-        return await super().create(obj_data)
+    async def get(self, id: UUID) -> Request:
+        request = await self.get_or_none(id)
+        if request is None:
+            # FIXME: написать и использовать кастомное исключение
+            raise LookupError(f"Объект Request c {id=} не найден.")
+        return request
 
-    async def update(self):
-        raise NotImplementedError(f"Метод `update` не определен в репозитории {self.__class__.__name__}")
+    async def create(self, request: Request) -> Request:
+        self.session.add(request)
+        await self.session.commit()
+        await self.session.refresh(request)
+        return request
+
+    async def update(self, id: UUID, request: Request) -> Request:
+        request.id = id
+        await self.session.merge(request)
+        await self.session.commit()
+        return request
