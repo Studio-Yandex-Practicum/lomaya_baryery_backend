43aae23dd2e6ec127bc745836df5db4636e85383
Ivan Skvortsov <pprofcheg@gmail.com>
2022-09-29 12:12:23 +0300

    removed logic from AbstracTrrepository methods

--- src/core/db/repository/abstract_repository.py
+++ src/core/db/repository/abstract_repository.py
@@ -1,43 +1,35 @@
 import abc
+from typing import Optional, TypeVar
 from uuid import UUID
 
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel as PydanticModel
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.core.db.models import Base as DatabaseModel
+DatabaseModel = TypeVar("DatabaseModel")
 
 
 class AbstractRepository(abc.ABC):
-    """Абстрактный класс, реализующий паттерн Repository."""
+    """Абстрактный класс, для реализации паттерна Repository."""
 
     @abc.abstractmethod
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: AsyncSession) -> None:
         self.session = session
-        self.model = DatabaseModel
 
     @abc.abstractmethod
-    async def get(self, obj_id: UUID) -> DatabaseModel:
-        return await self.session.scalar(select(self.model).where(self.model.id == obj_id))
+    async def get_or_none(self, id: UUID) -> Optional[DatabaseModel]:
+        """Получает из базы инстанс модели по ID. В случае отсутствия возвращает None."""
+        pass
 
     @abc.abstractmethod
-    async def create(self, obj_data: PydanticModel) -> DatabaseModel:
-        obj_data = obj_data.dict()
-        db_obj = self.model(**obj_data)
-        self.session.add(db_obj)
-        await self.session.commit()
-        await self.session.refresh(db_obj)
-        return db_obj
+    async def get(self, id: UUID) -> DatabaseModel:
+        """Получает инстанс модели по ID. В случае отсутствия инстанса бросает ошибку."""
+        pass
 
     @abc.abstractmethod
-    async def update(self, db_obj: DatabaseModel, obj_update_data: PydanticModel) -> DatabaseModel:
-        obj_current_data = jsonable_encoder(db_obj)
-        obj_update_data = obj_update_data.dict(exclude_unset=True)
-        for field in obj_current_data:
-            for field in obj_update_data:
-                setattr(db_obj, field, obj_update_data[field])
-        self.session.add(db_obj)
-        await self.session.commit()
-        await self.session.refresh(db_obj)
-        return db_obj
+    async def create(self, instance: DatabaseModel) -> DatabaseModel:
+        """Создает новый инстанс модели и сохраняет в базе."""
+        pass
+
+    @abc.abstractmethod
+    async def update(self, id: UUID, instance: DatabaseModel) -> DatabaseModel:
+        """Обновляет существующий инстанс модели в базе."""
+        pass
