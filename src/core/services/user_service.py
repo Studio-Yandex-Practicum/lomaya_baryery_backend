4c1253075cde8848b1049b1ad8e5c3599a6d6620
AlxShvalev <shvalevalx@gmail.com>
2022-10-01 11:35:40 +0500

    Added UserService

---
+++ src/core/services/user_service.py
@@ -0,0 +1,22 @@
+from typing import Union
+from uuid import UUID
+
+from fastapi import Depends
+
+from src.core.db.repository.shift_repository import ShiftRepository
+from src.core.db.repository.user_repository import UserRepository
+
+
+class UserService:
+    def __init__(
+        self, shift_repository: ShiftRepository = Depends(), user_repository: UserRepository = Depends()
+    ) -> None:
+        self.shift_repository = shift_repository
+        self.user_repository = user_repository
+
+    async def get_user_list_by_shift_id(self, shift_id: UUID) -> dict[str, Union[dict, list]]:
+        shift = await self.shift_repository.get(
+            shift_id,
+        )
+        users = await self.user_repository.get_user_list_by_shift_id(shift_id)
+        return {"shift": shift, "users": users}
