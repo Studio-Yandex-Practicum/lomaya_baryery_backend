566457733e5af1ceea3b0657243495b8584b282d
AlxShvalev <shvalevalx@gmail.com>
2022-10-02 19:07:55 +0500

    Move get shift users logic to UserService

--- src/core/services/user_service.py
+++ src/core/services/user_service.py
@@ -1,21 +1,31 @@
 from uuid import UUID
 
 from fastapi import Depends
+from sqlalchemy import and_, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.response_models.shift import ShiftUsersResponse
+from src.core.db.models import Request, User
 from src.core.db.repository.shift_repository import ShiftRepository
 from src.core.db.repository.user_repository import UserRepository
 
 
 class UserService:
     def __init__(
-        self, shift_repository: ShiftRepository = Depends(), user_repository: UserRepository = Depends()
+        self,
+        session: AsyncSession = Depends(),
+        shift_repository: ShiftRepository = Depends(),
+        user_repository: UserRepository = Depends(),
     ) -> None:
         self.shift_repository = shift_repository
+        self.session = session
         self.user_repository = user_repository
 
     async def get_user_list_by_shift_id(self, shift_id: UUID) -> ShiftUsersResponse:
         """Получаем смену и список пользователей зарегистрированных на смену по shift_id."""
         shift = await self.shift_repository.get(shift_id)
-        users = await self.user_repository.get_user_list_by_shift_id(shift_id)
+        # users = await self.user_repository.get_user_list_by_shift_id(shift_id)
+        statement = select(User).where(and_(Request.user_id == User.id, Request.shift_id == shift_id))
+        users = await self.session.execute(statement)
+        users = users.scalars().all()
         return ShiftUsersResponse(shift=shift, users=users)
