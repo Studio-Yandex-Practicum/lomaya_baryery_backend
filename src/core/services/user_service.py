2c0c80abf061f0a8d83c2757b29701356803e8a0
AlxShvalev <shvalevalx@gmail.com>
2022-10-02 19:31:46 +0500

    Added get_session dependensy to UserService

--- src/core/services/user_service.py
+++ src/core/services/user_service.py
@@ -5,26 +5,23 @@ from sqlalchemy import and_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.response_models.shift import ShiftUsersResponse
+from src.core.db.db import get_session
 from src.core.db.models import Request, User
 from src.core.db.repository.shift_repository import ShiftRepository
-from src.core.db.repository.user_repository import UserRepository
 
 
 class UserService:
     def __init__(
         self,
-        session: AsyncSession = Depends(),
+        session: AsyncSession = Depends(get_session),
         shift_repository: ShiftRepository = Depends(),
-        user_repository: UserRepository = Depends(),
     ) -> None:
         self.shift_repository = shift_repository
         self.session = session
-        self.user_repository = user_repository
 
     async def get_user_list_by_shift_id(self, shift_id: UUID) -> ShiftUsersResponse:
         """Получаем смену и список пользователей зарегистрированных на смену по shift_id."""
         shift = await self.shift_repository.get(shift_id)
-        # users = await self.user_repository.get_user_list_by_shift_id(shift_id)
         statement = select(User).where(and_(Request.user_id == User.id, Request.shift_id == shift_id))
         users = await self.session.execute(statement)
         users = users.scalars().all()
