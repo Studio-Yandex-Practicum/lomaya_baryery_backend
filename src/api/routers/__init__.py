bd65093cf90dac3dfd8e8cfbe8b9b09c57394cad
Bogdan Stefanyuk <86630295+Ponimon4ik@users.noreply.github.com>
2022-10-05 14:22:43 +0400

    Fixed import

--- src/api/routers/__init__.py
+++ src/api/routers/__init__.py
@@ -1,4 +1,4 @@
-from src.api.hello import TELEGRAM_WEBHOOK_ENDPOINT, router, webhook_router  # noqa
+from src.api.routers.hello import TELEGRAM_WEBHOOK_ENDPOINT, router, webhook_router  # noqa
 from src.api.routers.request import router as request_router  # noqa
-from src.api.shift import router as shift_router  # noqa
-from src.api.user_tasks import router as user_tasks  # noqa
+from src.api.routers.shift import router as shift_router  # noqa
+from src.api.routers.user_tasks import router as user_tasks  # noqa
