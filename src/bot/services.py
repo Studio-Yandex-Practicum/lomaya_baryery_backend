02f389d4d2f728c72ad0da6e04ee28fecf2d5a6e
Mariya Zhuchina <zhuchina@yandex.ru>
2022-10-10 13:39:52 +0300

    added saving photo report

--- src/bot/services.py
+++ src/bot/services.py
@@ -1,5 +1,11 @@
+import os
+
+from telegram import Update
+from telegram.ext import CallbackContext
+
 from src.core import settings
 from src.core.db import models
+from src.core.settings import BASE_DIR
 from src.bot.main import create_bot
 
 bot = create_bot().bot  # временная копия бота до миграции на webhooks
@@ -22,3 +28,16 @@ async def send_rejection_callback(user: models.User):
             f"{settings.ORGANIZATIONS_GROUP}"
         ),
     )
+
+
+async def download_photo_report_callback(update: Update, context: CallbackContext) -> str:
+    """Сохранить фото отчёта на диск в /data/user_reports."""
+    USER_REPORTS_DIR = os.path.join(BASE_DIR, settings.settings.DATA_DIR, settings.settings.USER_REPORTS_DIR)
+
+    if not os.path.exists(USER_REPORTS_DIR):
+        os.makedirs(USER_REPORTS_DIR)
+    file = await context.bot.get_file(update.message.photo[-1].file_id)
+    _, ext = os.path.splitext(file.file_path)
+    file_name = file.file_unique_id.replace('-', '') + ext
+    await file.download(custom_path=os.path.join(USER_REPORTS_DIR, file_name))
+    return str(file.file_path)
--- src/core/services/user_service.py
+++ src/core/services/user_service.py
@@ -26,3 +26,7 @@ class UserService:
             request_scheme = RequestCreateRequest(user_id=user.id, status=Request.Status.PENDING)
             request = Request(**request_scheme.dict())
             await self.user_repository.create(request)
+
+    async def get_user_by_telegram_id(self, telegram_id: int) -> User:
+        """Получить участника проекта по его telegram_id."""
+        return await self.user_repository.get_by_telegram_id(telegram_id)
