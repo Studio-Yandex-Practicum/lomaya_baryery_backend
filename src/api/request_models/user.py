ea12f5dae0d4a80f0dc12905652c4fd0dacb1c84
Pushkarev Anton <pushkarevantona@gmail.com>
2022-10-21 00:09:12 +0300

    personal data validation refactored

--- src/api/request_models/user.py
+++ src/api/request_models/user.py
@@ -1,57 +1,18 @@
-from datetime import date
 from typing import Optional
 from uuid import UUID
 
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, PastDate
 
 from src.core.db.models import Request
-
-INVALID_TYPE_ERROR = 'В поле {} передан некорректный тип данных.'.format
 
 
 class UserCreateRequest(BaseModel):
     telegram_id: int
     name: str = Field(min_length=2, max_length=100)
     surname: str = Field(min_length=2, max_length=100)
-    date_of_birth: date
+    date_of_birth: PastDate
     city: str = Field(min_length=2, max_length=50)
     phone_number: str
-
-    @validator("telegram_id", pre=True)
-    def validate_telegram_id(cls, value: int):
-        if not isinstance(value, int):
-            raise ValueError(INVALID_TYPE_ERROR('Telegram ID'))
-        return value
-
-    @validator("name", pre=True)
-    def validate_name(cls, value: str):
-        if not isinstance(value, str):
-            raise ValueError(INVALID_TYPE_ERROR('Имя'))
-        return value
-
-    @validator("surname", pre=True)
-    def validate_surname(cls, value: str):
-        if not isinstance(value, str):
-            raise ValueError(INVALID_TYPE_ERROR('Фамилия'))
-        return value
-
-    @validator("date_of_birth", pre=True)
-    def validate_date_of_birth(cls, value: date):
-        if not isinstance(value, date):
-            raise ValueError(INVALID_TYPE_ERROR('Дата рождения'))
-        return value
-
-    @validator("city", pre=True)
-    def validate_city(cls, value: str):
-        if not isinstance(value, str):
-            raise ValueError(INVALID_TYPE_ERROR('Город'))
-        return value
-
-    @validator("phone_number", pre=True)
-    def validate_phone_number(cls, value: str):
-        if not isinstance(value, str):
-            raise ValueError(INVALID_TYPE_ERROR('Телефон'))
-        return value
 
 
 class RequestCreateRequest(BaseModel):
--- src/core/services/user_service.py
+++ src/core/services/user_service.py
@@ -4,7 +4,6 @@ from datetime import date, datetime
 from fastapi import Depends
 from phonenumbers import PhoneNumberFormat, format_number, is_valid_number, parse
 from phonenumbers.phonenumberutil import NumberParseException
-from pydantic import validator
 
 from src.api.request_models.user import RequestCreateRequest, UserCreateRequest
 from src.core.db.models import Request, User
@@ -13,49 +12,60 @@ from src.core.db.repository.user_repository import UserRepository
 from src.core.settings import settings
 
 VALID_TEXT = "^[а-яА-ЯёЁ][а-яА-ЯёЁ -]*[а-яА-ЯёЁ]$"
-INVALID_TEXT_ERROR = "В поле {} использованы недопустимые символы.".format
+INVALID_TEXT_ERROR = "В поле {} могут быть использованы только русские буквы и \"-\".".format
 
 
-class ValidateUserCreate(UserCreateRequest):
+def validate_name(value: str) -> str:
+    """Валидация имени пользователя."""
+    correct_value = re.compile(VALID_TEXT).search(value)
+    if not correct_value:
+        raise ValueError(INVALID_TEXT_ERROR('Имя'))
+    return value.title()
+
+
+def validate_surname(value: str) -> str:
+    """Валидация фамилии пользователя."""
+    correct_value = re.compile(VALID_TEXT).search(value)
+    if not correct_value:
+        raise ValueError(INVALID_TEXT_ERROR('Фамилия'))
+    return value.title()
+
+
+def validate_date_of_birth(value: date) -> date:
+    """Валидация даты рождения пользователя."""
+    current_year = datetime.now().date().year
+    if current_year - value.year < settings.MIN_AGE:
+        raise ValueError(f'Возраст не может быть менее {settings.MIN_AGE} лет.')
+    return value
+
+
+def validate_city(value: str) -> str:
+    """Валидация города проживания пользователя."""
+    correct_value = re.compile(VALID_TEXT).search(value)
+    if not correct_value:
+        raise ValueError(INVALID_TEXT_ERROR('Город'))
+    return value.title()
+
+
+def validate_phone_number(value: str) -> str:
+    """Валидация и форматирование телефонного номера пользователя."""
+    try:
+        parsed_number = parse(value.replace('+', ''), "RU")
+    except NumberParseException:
+        raise ValueError('Некорректный номер телефона.')
+    if not is_valid_number(parsed_number):
+        raise ValueError('Некорректный номер телефона.')
+    return format_number(parsed_number, PhoneNumberFormat.E164)[1:]
+
+
+def ValidateUserCreate(user: UserCreateRequest) -> UserCreateRequest:
     """Валидация персональных данных пользователя."""
-
-    @validator("name")
-    def validate_name(cls, value: str):
-        correct_value = re.compile(VALID_TEXT).search(value)
-        if not correct_value:
-            raise ValueError(INVALID_TEXT_ERROR('Имя'))
-        return value.title()
-
-    @validator("surname")
-    def validate_surname(cls, value: str):
-        correct_value = re.compile(VALID_TEXT).search(value)
-        if not correct_value:
-            raise ValueError(INVALID_TEXT_ERROR('Фамилия'))
-        return value.title()
-
-    @validator("date_of_birth")
-    def validate_date_of_birth(cls, value: date):
-        current_year = datetime.now().date().year
-        if current_year - value.year < settings.MIN_AGE:
-            raise ValueError(f'Возраст не может быть менее {settings.MIN_AGE} лет.')
-        return value
-
-    @validator("city")
-    def validate_city(cls, value: str):
-        correct_value = re.compile(VALID_TEXT).search(value)
-        if not correct_value:
-            raise ValueError(INVALID_TEXT_ERROR('Город'))
-        return value.title()
-
-    @validator("phone_number", pre=True)
-    def validate_phone_number(cls, value: str):
-        try:
-            parsed_number = parse(value.replace('+', ''), "RU")
-        except NumberParseException:
-            raise ValueError('Некорректный номер телефона.')
-        if not is_valid_number(parsed_number):
-            raise ValueError('Некорректный номер телефона.')
-        return format_number(parsed_number, PhoneNumberFormat.E164)[1:]
+    user.name = validate_name(user.name)
+    user.surname = validate_surname(user.surname)
+    user.date_of_birth = validate_date_of_birth(user.date_of_birth)
+    user.city = validate_city(user.city)
+    user.phone_number = validate_phone_number(user.phone_number)
+    return user
 
 
 class UserService:
@@ -68,7 +78,7 @@ class UserService:
     async def user_registration(self, user_data: dict):
         """Регистрация пользователя. Отправка запроса на участие в смене."""
         user_scheme = UserCreateRequest(**user_data)
-        valid_user_scheme = ValidateUserCreate(**user_scheme.dict())
+        valid_user_scheme = ValidateUserCreate(user_scheme)
         user = await self.user_repository.get_by_telegram_id(valid_user_scheme.telegram_id)
         if not user:
             user = await self.user_repository.get_by_phone_number(valid_user_scheme.phone_number)
