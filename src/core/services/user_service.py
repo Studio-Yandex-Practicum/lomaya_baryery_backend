3f38134f66993f9bcd25489515f388c8cc1fe314
Pushkarev Anton <pushkarevantona@gmail.com>
2022-10-22 01:52:31 +0300

    user_service validation - partially updated

--- src/core/services/user_service.py
+++ src/core/services/user_service.py
@@ -15,19 +15,11 @@ VALID_TEXT = "^[а-яА-ЯёЁ][а-яА-ЯёЁ -]*[а-яА-ЯёЁ]$"
 INVALID_TEXT_ERROR = "В поле {} могут быть использованы только русские буквы и \"-\".".format
 
 
-def validate_name(value: str) -> str:
+def validate_text(value: str, field_name: str) -> str:
     """Валидация имени пользователя."""
     correct_value = re.compile(VALID_TEXT).search(value)
     if not correct_value:
-        raise ValueError(INVALID_TEXT_ERROR('Имя'))
-    return value.title()
-
-
-def validate_surname(value: str) -> str:
-    """Валидация фамилии пользователя."""
-    correct_value = re.compile(VALID_TEXT).search(value)
-    if not correct_value:
-        raise ValueError(INVALID_TEXT_ERROR('Фамилия'))
+        raise ValueError(INVALID_TEXT_ERROR(field_name))
     return value.title()
 
 
@@ -37,14 +29,6 @@ def validate_date_of_birth(value: date) -> date:
     if current_year - value.year < settings.MIN_AGE:
         raise ValueError(f'Возраст не может быть менее {settings.MIN_AGE} лет.')
     return value
-
-
-def validate_city(value: str) -> str:
-    """Валидация города проживания пользователя."""
-    correct_value = re.compile(VALID_TEXT).search(value)
-    if not correct_value:
-        raise ValueError(INVALID_TEXT_ERROR('Город'))
-    return value.title()
 
 
 def validate_phone_number(value: str) -> str:
@@ -58,27 +42,23 @@ def validate_phone_number(value: str) -> str:
     return format_number(parsed_number, PhoneNumberFormat.E164)[1:]
 
 
-async def validate_user_exists(
+async def validate_user_not_exists(
     user_repository: UserRepository, telegram_id: int = None, phone_number: str = None
 ) -> None:
     """Проверка, что в БД нет пользователя с указанным telegram_id или phone_number."""
-    user = None
-    if telegram_id:
-        user = await user_repository.get_by_telegram_id(telegram_id)
-    if not user and phone_number:
-        user = await user_repository.get_by_phone_number(phone_number)
-    if user:
-        raise ValueError(f'Пользователь уже зарегистрирован. Telegram ID: {user.telegram_id}.')
+    user_exists = await user_repository.check_user_existence(telegram_id, phone_number)
+    if user_exists:
+        raise ValueError('Пользователь уже зарегистрирован.')
 
 
-async def ValidateUserCreate(user: UserCreateRequest, user_repository: UserRepository) -> UserCreateRequest:
+async def validate_user_create(user: UserCreateRequest, user_repository: UserRepository) -> UserCreateRequest:
     """Валидация персональных данных пользователя."""
-    user.name = validate_name(user.name)
-    user.surname = validate_surname(user.surname)
+    user.name = validate_text(user.name, 'Имя')
+    user.surname = validate_text(user.surname, 'Фамилия')
     user.date_of_birth = validate_date_of_birth(user.date_of_birth)
-    user.city = validate_city(user.city)
+    user.city = validate_text(user.city, 'Город')
     user.phone_number = validate_phone_number(user.phone_number)
-    await validate_user_exists(user_repository, user.telegram_id, user.phone_number)
+    await validate_user_not_exists(user_repository, user.telegram_id, user.phone_number)
     return user
 
 
@@ -92,7 +72,7 @@ class UserService:
     async def user_registration(self, user_data: dict):
         """Регистрация пользователя. Отправка запроса на участие в смене."""
         user_scheme = UserCreateRequest(**user_data)
-        valid_user_scheme = await ValidateUserCreate(user_scheme, self.user_repository)
+        valid_user_scheme = await validate_user_create(user_scheme, self.user_repository)
         user = User(**valid_user_scheme.dict())
         await self.user_repository.create(user)
         request = await self.request_repository.get_or_none(user.id)
