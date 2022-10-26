25da3520417b92061dc8e9d00752a8f9f6222bb1
pro100cemuk <pro100cemuk@gmail.com>
2022-10-26 18:02:29 +0300

    .env.example added global url, changed methods and their names in healthcheck_service.py, added method in user_task_repository, fixed bugs

--- src/api/response_models/healthcheck.py
+++ src/api/response_models/healthcheck.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class HealthcheckResponse(BaseModel):
     """Модель для вывода статусов ручки /healthcheck."""
 
-    bot_status: str
-    api_status: str
-    db_status: str
+    bot_status: tuple[bool, Optional[str]]
+    api_status: tuple[bool, Optional[str]]
+    db_status: tuple[bool, Optional[str]]
