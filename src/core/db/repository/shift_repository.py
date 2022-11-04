3a457407902eaecbe480454d7ce78cd84aeff0fe
pro100cemuk <pro100cemuk@gmail.com>
2022-11-04 14:54:10 +0300

    added custom api exceptions and handler

--- src/core/db/repository/shift_repository.py
+++ src/core/db/repository/shift_repository.py
@@ -36,7 +36,7 @@ class ShiftRepository(AbstractRepository):
 
     async def update(self, id: UUID, shift: Shift) -> Shift:
         shift.id = id
-        shift = await self.session.merge(shift)
+        await self.session.merge(shift)
         await self.session.commit()
         return shift
 
