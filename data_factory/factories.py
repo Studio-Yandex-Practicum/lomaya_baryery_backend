4b4b059294d622eb31f6a7cbb9bdf705067e098c
elit <elityaev@yandex.ru>
2022-11-29 09:44:10 +0300

    refact data_factory v.2

--- data_factory/factories.py
+++ data_factory/factories.py
@@ -3,15 +3,15 @@ from datetime import timedelta
 import random
 
 import factory
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, func
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from src.core.db import models
-from src.core.db.models import Shift
+from src.core.db.models import Shift, UserTask, Task, Photo
 from src.core.settings import settings
 
 STARTED_SHIFT_DURATION = 30
-MAX_USER_BIRTH_DATE = datetime.date(2012, 1, 1)
+MAX_USER_BIRTH_DATE = datetime.date(1986, 1, 1)
 MIN_USER_BIRTH_DATE = datetime.date(2016, 1, 1)
 
 engine = create_engine(settings.database_url.replace("+asyncpg", "+psycopg2"))
@@ -37,7 +37,7 @@ class UserFactory(BaseFactory):
     city = factory.Iterator(["Москва", "Санкт-Петербург", "Казань", "Нижний Новгород", "Екатеринбург", "Хабаровск"])
     phone_number = factory.Sequence(lambda n: str(89991234567 + n))
     telegram_id = factory.Sequence(lambda n: 123556787 + n)
-    numbers_lombaryers = 0
+    numbers_lombaryers = factory.Faker("random_int", min=0, max=92)
 
 
 class ShiftFactory(BaseFactory):
@@ -69,7 +69,7 @@ class ShiftFactory(BaseFactory):
 
     @factory.lazy_attribute
     def finished_at(self):
-        finished_at = self.started_at + timedelta(days=random.randrange(90, 93))
+        finished_at = self.started_at + timedelta(days=90)
         return finished_at
 
 
@@ -92,12 +92,54 @@ class RequestFactory(BaseFactory):
     class Meta:
         model = models.Request
 
+    id = factory.Faker("uuid4")
     status = factory.Iterator([status for status in models.Request.Status])
     numbers_lombaryers = factory.Faker("random_int", min=0, max=92)
+
+    @factory.post_generation
+    def add_several_user_tasks(self, created, count, **kwargs):
+        start_date = SESSION.execute(select(Shift.started_at).where(Shift.id == self.shift_id))
+        start_date = start_date.scalars().first()
+        all_dates = list((start_date + timedelta(day)) for day in range(91))
+        for date in all_dates:
+            if created and count:
+                UserTaskFactory.create_batch(
+                    count,
+                    user_id=self.user_id,
+                    shift_id=self.shift_id,
+                    task_date=date,
+                )
+
+    @classmethod
+    def complex_create(cls, count, **kwargs):
+        return cls.create_batch(
+            count,
+            add_several_user_tasks=1,
+            **kwargs,
+        )
 
 
 class UserTaskFactory(BaseFactory):
     class Meta:
         model = models.UserTask
 
-    status = factory.Iterator([status for status in models.UserTask.Status])
+    @factory.lazy_attribute
+    def status(self):
+        if self.task_date < datetime.date.today():
+            return UserTask.Status.APPROVED
+        if self.task_date == datetime.date.today():
+            return UserTask.Status.UNDER_REVIEW
+        else:
+            return UserTask.Status.NEW
+
+    @factory.lazy_attribute
+    def task_id(self):
+        task_ids = SESSION.execute(
+            select(Task.id).order_by(func.random()))
+        return task_ids.scalars().first()
+
+    @factory.lazy_attribute
+    def photo_id(self):
+        photo_ids = SESSION.execute(
+            select(Photo.id).order_by(func.random()))
+        return photo_ids.scalars().first()
--- data_factory/main.py
+++ data_factory/main.py
@@ -1,41 +1,18 @@
 import logging
-import random
+
 import sys
-from datetime import timedelta
 
 import click
 import factory
-
 from sqlalchemy import select, func
+
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
-from data_factory.factories import (SESSION, RequestFactory,
-                                    UserTaskFactory, UserFactory,
-                                    ShiftFactory, PhotoFactory, TaskFactory, STARTED_SHIFT_DURATION)
+from data_factory.factories import (SESSION, UserFactory,
+                                    ShiftFactory, PhotoFactory, TaskFactory, RequestFactory)
 
-from src.core.db.models import User, Task, UserTask, Request, Shift
-
-FAKE_USERS_COUNT = 200
-FAKE_TASK_COUNT = 31
-FAKE_PHOTO_COUNT = 10
-FAKE_FINISHED_SHIFTS_COUNT = 1
-FAKE_TOTAL_REQUESTS_IN_SHIFT = random.randrange(30, 60)
-FAKE_REQUESTS_DECLINED_COUNT = range(2)
-FAKE_REQUESTS_EXCLUDED_COUNT = range(2, 4)
-FAKE_REQUESTS_APPROVED_COUNT = range(4, FAKE_TOTAL_REQUESTS_IN_SHIFT)
-STATUS_REQUESTS_COUNT = {
-    Request.Status.DECLINED: FAKE_REQUESTS_DECLINED_COUNT,
-    Request.Status.EXCLUDED: FAKE_REQUESTS_EXCLUDED_COUNT,
-    Request.Status.APPROVED: FAKE_REQUESTS_APPROVED_COUNT
-}
-
-FAKE_PREPARING_REQUESTS_DECLINED_COUNT = range(2)
-FAKE_PREPARING_REQUESTS_APPROVED_COUNT = range(2, FAKE_TOTAL_REQUESTS_IN_SHIFT)
-STATUS_PREPARING_REQUESTS_COUNT = {
-    Request.Status.DECLINED: FAKE_REQUESTS_DECLINED_COUNT,
-    Request.Status.APPROVED: FAKE_REQUESTS_APPROVED_COUNT
-}
+from src.core.db.models import Shift, User, Request
 
 
 def get_logger(log_filename: str) -> logging.Logger:
@@ -51,112 +28,14 @@ def get_logger(log_filename: str) -> logging.Logger:
 logger = get_logger('fill_db.log')
 
 
-def generate_started_shifts(task_ids_list: list) -> None:
-    logger.info("Заполнение данными для активной смены...")
-    started_shift = ShiftFactory.create(status=Shift.Status.STARTED)
-    user_ids = SESSION.execute(select(User.id).order_by(func.random()).limit(FAKE_TOTAL_REQUESTS_IN_SHIFT))
+def get_random_user_ids(count: int) -> list:
+    user_ids = SESSION.execute(select(User.id).order_by(func.random()).limit(count))
     user_ids = user_ids.scalars().all()
-
-    logger.info("Создание заявок для активной смены...")
-    for status, count in STATUS_REQUESTS_COUNT.items():
-        for i in count:
-            RequestFactory.create(status=status, user_id=user_ids[i], shift_id=started_shift.id)
-    user_ids = SESSION.execute(select(Request.user_id).where(
-        Request.status == Request.Status.APPROVED, Request.shift_id == started_shift.id)
-    )
-    logger.info("Создание создание заданий для участников активной смены...")
-    user_ids = user_ids.scalars().all()
-    number_days = (started_shift.finished_at - started_shift.started_at).days
-    all_dates = tuple((started_shift.started_at + timedelta(day)) for day in range(number_days))
-    for user_id in user_ids:
-        for one_date in all_dates[:STARTED_SHIFT_DURATION]:
-            UserTaskFactory.create(
-                task_id=task_ids_list[one_date.day - 1],
-                user_id=user_id,
-                shift_id=started_shift.id,
-                task_date=one_date,
-                status=UserTask.Status.APPROVED
-            )
-            user = SESSION.execute(select(User).where(User.id == user_id))
-            user = user.scalars().first()
-            user.numbers_lombaryers += 1
-        UserTaskFactory.create(
-            task_id=task_ids_list[one_date.day - 1],
-            user_id=user_id,
-            shift_id=started_shift.id,
-            task_date=all_dates[STARTED_SHIFT_DURATION],
-            status=UserTask.Status.UNDER_REVIEW
-        )
-        for one_date in all_dates[STARTED_SHIFT_DURATION + 1:]:
-            UserTaskFactory.create(
-                task_id=task_ids_list[one_date.day - 1],
-                user_id=user_id,
-                shift_id=started_shift.id,
-                task_date=one_date,
-                status=UserTask.Status.NEW
-            )
-
-
-def generate_preparing_shifts() -> None:
-    logger.info("Заполнение данными для новой смены...")
-    preparing_shift = ShiftFactory.create(status=Shift.Status.PREPARING)
-    user_ids = SESSION.execute(select(User.id).order_by(func.random()).limit(FAKE_TOTAL_REQUESTS_IN_SHIFT))
-    user_ids = user_ids.scalars().all()
-    logger.info("Создание заявок для новой смены")
-    for status, count in STATUS_REQUESTS_COUNT.items():
-        for i in count:
-            RequestFactory.create(status=status, user_id=user_ids[i], shift_id=preparing_shift.id)
-
-
-def generate_finished_shifts(task_ids_list: list) -> None:
-    logger.info("Заполнение данными для завершенных смен...")
-    finished_shifts = ShiftFactory.create_batch(FAKE_FINISHED_SHIFTS_COUNT, status=Shift.Status.FINISHED)
-    for finished_shift in finished_shifts:
-        user_ids = SESSION.execute(select(User.id).order_by(func.random()).limit(FAKE_TOTAL_REQUESTS_IN_SHIFT))
-        user_ids = user_ids.scalars().all()
-        logger.info("Создание заявок для завершенных смен...")
-        for status, count in STATUS_REQUESTS_COUNT.items():
-            for i in count:
-                RequestFactory.create(status=status, user_id=user_ids[i], shift_id=finished_shift.id)
-        user_ids = SESSION.execute(select(Request.user_id).where(
-            Request.status == Request.Status.APPROVED, Request.shift_id == finished_shift.id)
-        )
-        user_ids = user_ids.scalars().all()
-        number_days = (finished_shift.finished_at - finished_shift.started_at).days
-        all_dates = tuple((finished_shift.started_at + timedelta(day)) for day in range(number_days))
-        logger.info("Создание заданий для участников завершенных смен...")
-        for user_id in user_ids:
-            for one_date in all_dates[:2]:
-                UserTaskFactory.create(
-                    task_id=task_ids_list[one_date.day - 1],
-                    user_id=user_id,
-                    shift_id=finished_shift.id,
-                    task_date=one_date,
-                    status=UserTask.Status.DECLINED
-                )
-            for one_date in all_dates[3:]:
-                UserTaskFactory.create(
-                    task_id=task_ids_list[one_date.day - 1],
-                    user_id=user_id,
-                    shift_id=finished_shift.id,
-                    task_date=one_date,
-                    status=UserTask.Status.APPROVED
-                )
-                user = SESSION.execute(select(User).where(User.id == user_id))
-                user = user.scalars().first()
-                user.numbers_lombaryers = 0
-                user.numbers_lombaryers += 1
+    return user_ids
 
 
 def truncate_tables(session: Session) -> None:
     """Очистить таблицы БД."""
-    msg = (
-        "ВНМАНИЕ! Дальнейшее действие приведет к удалению ВСЕХ существующих данных из ВСЕХ таблиц БД!\n"
-        "Продолжить? (y/n): "
-    )
-    if input(msg).lower().strip() not in ("y", "yes"):
-        return
-    # print("Удаление данных из таблиц...")
     logger.info("Удаление данных из таблиц...")
     session.execute("""TRUNCATE TABLE photos, requests, shifts, tasks, user_tasks, users""")
     session.commit()
@@ -167,24 +46,46 @@ def generate_fake_data() -> None:
     with factory.Faker.override_default_locale("ru_RU"):
 
         logger.info("Создание пользователей...")
-        UserFactory.create_batch(FAKE_USERS_COUNT)
-        logger.info(f"Создано {FAKE_USERS_COUNT} пользователей")
+        UserFactory.create_batch(200)
 
         logger.info("Создание заданий...")
-        TaskFactory.create_batch(FAKE_TASK_COUNT)
-        logger.info(f"Создано {FAKE_TASK_COUNT} заданий")
+        TaskFactory.create_batch(31)
 
         logger.info("Создание фотографий...")
-        PhotoFactory.create_batch(FAKE_PHOTO_COUNT)
-        logger.info(f"Создано {FAKE_PHOTO_COUNT} фотографий")
+        PhotoFactory.create_batch(10)
 
-        task_ids_list = SESSION.execute(select(Task.id))
-        task_ids_list = task_ids_list.scalars().all()
-        random.shuffle(task_ids_list)
+        logger.info("Создание активной смены...")
+        started_shift = ShiftFactory.create(status=Shift.Status.STARTED)
+        user_ids = get_random_user_ids(30)
 
-        generate_started_shifts(task_ids_list)
-        generate_preparing_shifts()
-        generate_finished_shifts(task_ids_list)
+        logger.info("Создание одобренных заявок и заданий для активной смены...")
+        for user_id in user_ids[:15]:
+            RequestFactory.complex_create(1, user_id=user_id, shift_id=started_shift.id,
+                                          status=Request.Status.APPROVED)
+
+        logger.info("Создание отклоненных заявок для активной смены...")
+        for user_id in user_ids[15:]:
+            RequestFactory.create(user_id=user_id, shift_id=started_shift.id,
+                                  status=Request.Status.DECLINED)
+
+        logger.info("Создание завершенной смены...")
+        finished_shifts = ShiftFactory.create_batch(2, status=Shift.Status.FINISHED)
+        for finished_shift in finished_shifts:
+            user_ids = get_random_user_ids(30)
+
+            logger.info("Создание одобренных заявок и заданий для завершенной смены...")
+            for user_id in user_ids[:15]:
+                RequestFactory.complex_create(1, user_id=user_id, shift_id=finished_shift.id,
+                                              status=Request.Status.APPROVED)
+
+            logger.info("Создание отклоненных заявок для завершенной смены...")
+            for user_id in user_ids[15:]:
+                RequestFactory.create(user_id=user_id, shift_id=finished_shift.id,
+                                      status=Request.Status.DECLINED)
+
+        logger.info("Создание новой смены...")
+        ShiftFactory.create(status=Shift.Status.PREPARING)
+
     logger.info("Создание тестовых данных завершено!")
 
 
@@ -192,6 +93,12 @@ def generate_fake_data() -> None:
 @click.option('--delete', is_flag=True, help="Удаление существующих данных из таблиц")
 def fill_command(delete) -> None:
     if delete:
+        msg = (
+            "ВНМАНИЕ! Дальнейшее действие приведет к удалению ВСЕХ существующих данных из ВСЕХ таблиц БД!\n"
+            "Продолжить? (y/n): "
+        )
+        if input(msg).lower().strip() not in ("y", "yes"):
+            return
         truncate_tables(SESSION)
         generate_fake_data()
     else:
