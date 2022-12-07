2270bde4a1508a7ebde5d94aec51819b51008f86
AlxShvalev <shvalevalx@gmail.com>
2022-12-07 20:40:23 +0500

    Add validation to ShiftCreateRequest
    Added started_at, finished_at validation. Added min_length validation to shift title

--- src/api/request_models/shift.py
+++ src/api/request_models/shift.py
@@ -2,7 +2,7 @@ import enum
 from datetime import date, datetime, timedelta
 from typing import Optional
 
-from pydantic import Field, validator
+from pydantic import Field, root_validator, validator
 
 from src.api.request_models.request_base import RequestBase
 
@@ -18,7 +18,7 @@ class ShiftSortRequest(str, enum.Enum):
 class ShiftCreateRequest(RequestBase):
     started_at: datetime
     finished_at: datetime
-    title: str = Field(..., max_length=50)
+    title: str = Field(..., min_length=3, max_length=100)
 
     @validator("started_at")
     def validate_started_later_than_now(cls, value: datetime) -> datetime:
@@ -31,6 +31,12 @@ class ShiftCreateRequest(RequestBase):
         if value.date() > (date.today() + timedelta(days=120)):
             raise ValueError("Дата окончания не может быть больше 4-х месяцев")
         return value
+
+    @root_validator(skip_on_failure=True)
+    def validate_finished_at_earlier_then_started(cls, values: dict) -> dict:
+        if values["started_at"] > values["finished_at"]:
+            raise ValueError("Дата начала смены не может быть больше даты окончания")
+        return values
 
 
 class ShiftUpdateRequest(ShiftCreateRequest):
