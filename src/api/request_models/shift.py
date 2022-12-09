c172a94dff9ec0668b1294c1f8844f6842cc642a
DIABLik666 <di-devil@yandex.ru>
2022-12-09 18:01:29 +0700

    Added validators

--- src/api/request_models/shift.py
+++ src/api/request_models/shift.py
@@ -2,7 +2,7 @@ import enum
 from datetime import date, datetime, timedelta
 from typing import Optional
 
-from pydantic import Field, validator
+from pydantic import Field, root_validator, validator
 
 from src.api.request_models.request_base import RequestBase
 
@@ -32,9 +32,30 @@ class ShiftCreateRequest(RequestBase):
             raise ValueError("Дата окончания не может быть больше 4-х месяцев")
         return value
 
+    @root_validator(skip_on_failure=True)
+    def validate_started_at_and_finished_at_fields_together(cls, values):
+        if values['finished_at'].date() <= values['started_at'].date():
+            raise ValueError("Дата окончания не может быть меньше или равняться дате начала")
+        return values
+
 
 class ShiftUpdateRequest(ShiftCreateRequest):
     started_at: Optional[datetime] = Field(None)
     finished_at: Optional[datetime] = Field(None)
     title: Optional[str] = Field(None, max_length=100)
     final_message: Optional[str] = Field(None, max_length=400)
+
+    @validator("final_message")
+    def validate_final_message_field(cls, value: str):
+        if value.isnumeric():
+            raise ValueError("Финальное сообщение не может состоять только из цифр")
+        return value
+
+    @root_validator(skip_on_failure=True)
+    def validate_started_at_and_finished_at_fields_together(cls, values):
+        fields_in_request = values['started_at'], values['finished_at']
+        if all(fields_in_request):
+            return super().validate_started_at_and_finished_at_fields_together(values)
+        elif any(fields_in_request):
+            raise ValueError("Нельзя изменять даты начала и окончания отдельно друг от друга")
+        return values
