eea88e8d8adb869e3449b6488d18284d1dbf9a79
Ivan Skvortsov <pprofcheg@gmail.com>
2022-12-16 17:14:14 +0300

    added background_tasks to finish_shift for message sending

--- src/api/routers/shift.py
+++ src/api/routers/shift.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 from typing import Optional
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, BackgroundTasks, Depends
 from fastapi import Request as FastAPIRequest
 from fastapi_restful.cbv import cbv
 
@@ -191,9 +191,11 @@ class ShiftCBV:
         summary="Завершение смены",
         response_description="Информация о завершенной смене",
     )
-    async def finish_shift(self, request: FastAPIRequest, shift_id: UUID) -> ShiftResponse:
+    async def finish_shift(
+        self, request: FastAPIRequest, shift_id: UUID, background_tasks: BackgroundTasks
+    ) -> ShiftResponse:
         """Закончить смену.
 
         - **shift_id**: уникальный индентификатор смены
         """
-        return await self.shift_service.finish_shift(request.app.state.bot_instance.bot, shift_id)
+        return await self.shift_service.finish_shift(request.app.state.bot_instance.bot, shift_id, background_tasks)
