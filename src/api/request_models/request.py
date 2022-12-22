b8c68235f77e74db5362f714621eeb476f1e1292
AlxShvalev <shvalevalx@gmail.com>
2022-12-22 22:57:00 +0500

    Remove unused class RequestStatusUpdateRequest

--- src/api/request_models/request.py
+++ src/api/request_models/request.py
@@ -1,13 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
-
 from src.api.request_models.request_base import RequestBase
-from src.core.db.models import Request
-
-
-class RequestStatusUpdateRequest(BaseModel):
-    status: Request.Status = Field(Request.Status.APPROVED.value)
 
 
 class RequestDeclineRequest(RequestBase):
