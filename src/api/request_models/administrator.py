d191284bd9d6cf94cfac748a5186c55d16b5917f
Ivan Skvortsov <pprofcheg@gmail.com>
2022-12-26 12:07:56 +0300

    created request models, updated pyproject.toml

--- src/api/request_models/administrator.py
+++ src/api/request_models/administrator.py
@@ -1,12 +1,19 @@
+from pydantic import EmailStr, Field, SecretStr
+
 from src.api.request_models.request_base import RequestBase
 
 
 class AdministratorCreateRequest(RequestBase):
     """Схема для создания администратора."""
 
-    name: str
-    surname: str
+    name: str = Field(min_length=2, max_length=100)
+    surname: str = Field(min_length=2, max_length=100)
+    email: EmailStr
+    password: SecretStr = Field(min_length=8, max_length=50)
+
+
+class AdministratorAuthenticateRequest(RequestBase):
+    """Схема для аутентификации администратора."""
+
     email: str
     password: str
-
-    # TODO: валидация
