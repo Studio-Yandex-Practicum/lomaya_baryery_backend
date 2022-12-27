a690bc77506db88b1aa4a987f915cf53fc03630d
Arseniy Gaintsev <arseniy.gaintsev@gmail.com>
2022-12-27 20:43:20 +0300

    добавлен мердж из develop src/bot/api_services.py

--- src/bot/api_services.py
+++ src/bot/api_services.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-from src.core.db.db import get_session
 from src.core.db.repository import (
     MemberRepository,
     ReportRepository,
@@ -9,32 +8,37 @@ from src.core.db.repository import (
     TaskRepository,
     UserRepository,
 )
+from src.core.services.member_service import MemberService
 from src.core.services.report_service import ReportService
-from src.core.services.request_sevice import RequestService
+from src.core.services.task_service import TaskService
 from src.core.services.user_service import UserService
 
 
 async def get_registration_service_callback(sessions) -> Optional[UserService]:
-    async for session in sessions:  # noqa
+    async for session in sessions:  # noqa R503
         request_repository = RequestRepository(session)
         user_repository = UserRepository(session)
         registration_service = UserService(user_repository, request_repository)
         return registration_service
 
 
-async def get_report_service_callback():
-    async for session in get_session():
-        request_repository = RequestRepository(session)
+async def get_report_service_callback(sessions):
+    async for session in sessions:  # noqa R503
         shift_repository = ShiftRepository(session)
         task_repository = TaskRepository(session)
         report_repository = ReportRepository(session)
-        request_service = RequestService(request_repository)
         member_repository = MemberRepository(session)
+        task_service = TaskService(task_repository)
+        member_service = MemberService(member_repository, shift_repository)
         report_service = ReportService(
-            report_repository,
-            task_repository,
-            shift_repository,
-            request_service,
-            member_repository,
+            report_repository, shift_repository, member_repository, task_service, member_service
         )
-    return report_service
+        return report_service
+
+
+async def get_member_service_callback(sessions):
+    async for session in sessions:  # noqa R503
+        member_repository = MemberRepository(session)
+        shift_repository = ShiftRepository(session)
+        member_service = MemberService(member_repository, shift_repository)
+        return member_service
