1c8feca560bc735ca3280d303f2c2795986197ef
Ivan Skvortsov <pprofcheg@gmail.com>
2022-12-29 22:21:29 +0300

    refactored router imports

--- src/api/routers/__init__.py
+++ src/api/routers/__init__.py
@@ -1,10 +1,6 @@
 from src.api.routers.administrator import router as administrator_router  # noqa
 from src.api.routers.healthcheck import router as healthcheck_router  # noqa
-from src.api.routers.hello import (  # noqa
-    TELEGRAM_WEBHOOK_ENDPOINT,
-    router,
-    webhook_router,
-)
+from src.api.routers.hello import TELEGRAM_WEBHOOK_ENDPOINT, webhook_router  # noqa
 from src.api.routers.report import router as report_router  # noqa
 from src.api.routers.request import router as request_router  # noqa
 from src.api.routers.shift import router as shift_router  # noqa
