96527c09ef1dd836cb8e44eae7330d37c9b1bdfe
Vae Semper <70967111+VaeSemper@users.noreply.github.com>
2023-01-29 14:42:14 +0300

    refactoring: create error response module

---
+++ src/api/response_models/error.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class ErrorResponse(BaseModel):
+    detail: str
+
+
+def generate_error_responses(*error_status_codes: int) -> dict[int, dict[str, Any]]:
+    """Создает шаблоны ошибок из входящих статус кодов."""
+    error_responses = {code: {"desc": HTTPStatus(code).phrase, "model": ErrorResponse} for code in error_status_codes}
+    return error_responses
