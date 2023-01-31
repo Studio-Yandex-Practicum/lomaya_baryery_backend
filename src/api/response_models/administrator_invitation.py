82accd8f0da84a52861cd75374952adcb773cc7f
AlxShvalev <shvalevalx@gmail.com>
2023-01-31 18:22:02 +0500

    added AdministratorInvitationResponse

---
+++ src/api/response_models/administrator_invitation.py
@@ -0,0 +1,14 @@
+from datetime import date
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class AdministratorInvitationResponse(BaseModel):
+    """Схема для отображения информации о приглашенном администраторе."""
+
+    name: str
+    surname: str
+    email: str
+    expired_date: date
+    token: UUID
