db1bb160ad2ba11a92eab4bef99cc4b3719e9621
AlxShvalev <shvalevalx@gmail.com>
2023-02-01 21:35:53 +0500

    added AdministratorRegistrationRequest class

--- src/api/request_models/administrator.py
+++ src/api/request_models/administrator.py
@@ -1,10 +1,20 @@
-from pydantic import EmailStr, SecretStr
+from pydantic import EmailStr, Field, SecretStr
 
 from src.api.request_models.request_base import RequestBase
+from src.core.db.models import Administrator
 
 
 class AdministratorAuthenticateRequest(RequestBase):
     """Схема для аутентификации администратора."""
 
     email: EmailStr
+    password: SecretStr
+
+
+class AdministratorRegistrationRequest(RequestBase):
+    """Схема для регистрации администратора."""
+
+    name: str = Field(min_length=2, max_length=100)
+    surname: str = Field(min_length=2, max_length=100)
+    role: Administrator.Role
     password: SecretStr
