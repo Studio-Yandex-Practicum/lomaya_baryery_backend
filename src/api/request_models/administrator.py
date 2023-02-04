556dab4937d9d18909c342af8591cff5ff052f54
AlxShvalev <shvalevalx@gmail.com>
2023-02-04 15:28:50 +0500

    added token to AdministratorRegistrationRequest schema

--- src/api/request_models/administrator.py
+++ src/api/request_models/administrator.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from pydantic import EmailStr, Field, SecretStr
 
 from src.api.request_models.request_base import RequestBase
@@ -18,3 +20,4 @@ class AdministratorRegistrationRequest(RequestBase):
     surname: str = Field(min_length=2, max_length=100)
     role: Administrator.Role
     password: SecretStr
+    token: UUID
