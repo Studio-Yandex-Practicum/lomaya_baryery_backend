0d12b924d1a04fde5780c532d6c25a5a8de66ebd
AlxShvalev <shvalevalx@gmail.com>
2023-02-04 15:30:10 +0500

    added register_new_administrator method

--- src/core/services/administrator_service.py
+++ src/core/services/administrator_service.py
@@ -1,8 +1,36 @@
+import datetime
+
 from fastapi import Depends
 
-from src.core.db.repository import AdministratorRepository
+from src.api.request_models.administrator import AdministratorRegistrationRequest
+from src.api.response_models.administrator import AdministratorResponse
+from src.core.db.models import Administrator
+from src.core.db.repository import (
+    AdministratorInvitationRepository,
+    AdministratorRepository,
+)
+from src.core.services.authentication_service import PASSWORD_CONTEXT
 
 
 class AdministratorService:
-    def __init__(self, administrator_repository: AdministratorRepository = Depends()):
+    def __init__(
+        self,
+        administrator_repository: AdministratorRepository = Depends(),
+        administrator_invitation_repository: AdministratorInvitationRepository = Depends(),
+    ):
         self.__administrator_repository = administrator_repository
+        self.__administrator_invitation_repository = administrator_invitation_repository
+
+    async def register_new_administrator(self, schema: AdministratorRegistrationRequest) -> AdministratorResponse:
+        dataset = schema.dict()
+        token = dataset.pop("token", None)
+        password = dataset.pop("password", None)
+        invitation = await self.__administrator_invitation_repository.get_mail_request_by_token(token)
+        administrator = Administrator(**dataset)
+        administrator.status = Administrator.Status.ACTIVE
+        administrator.email = invitation.email
+        administrator.hashed_password = PASSWORD_CONTEXT.hash(password.get_secret_value())
+        invitation.expired_date = datetime.date.today() - datetime.timedelta(days=1)
+        await self.__administrator_repository.create(administrator)
+        await self.__administrator_invitation_repository.update(invitation.id, invitation)
+        return administrator
