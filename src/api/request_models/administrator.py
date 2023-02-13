755e24c0ea2f466a878af6b2f6cf09900523f741
AlxShvalev <shvalevalx@gmail.com>
2023-02-13 11:12:02 +0500

    added validators for name, surname

--- src/api/request_models/administrator.py
+++ src/api/request_models/administrator.py
@@ -1,6 +1,9 @@
-from pydantic import EmailStr, Field, SecretStr
+import re
+
+from pydantic import EmailStr, Field, SecretStr, validator
 
 from src.api.request_models.request_base import RequestBase
+from src.api.request_models.user import INVALID_TEXT_ERROR, VALID_TEXT
 from src.core.db.models import Administrator
 
 
@@ -17,6 +20,18 @@ class AdministratorRegistrationRequest(RequestBase):
     name: str = Field(min_length=2, max_length=100)
     surname: str = Field(min_length=2, max_length=100)
     password: SecretStr
+
+    @validator("name")
+    def validate_name(cls, value: str) -> str:
+        if not re.compile(VALID_TEXT).match(value):
+            raise ValueError(INVALID_TEXT_ERROR.format("Имя"))
+        return value.title()
+
+    @validator("surname")
+    def validate_surname(cls, value: str) -> str:
+        if not re.compile(VALID_TEXT).match(value):
+            raise ValueError(INVALID_TEXT_ERROR.format("Фамилия"))
+        return value.title()
 
     async def parse_to_db_obj(self) -> Administrator:
         administrator = Administrator()
