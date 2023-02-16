f340858251f132455f51a4c9310d980bcd2d3c7d
AlxShvalev <shvalevalx@gmail.com>
2023-02-16 20:15:49 +0500

    add password validation

--- src/api/request_models/administrator.py
+++ src/api/request_models/administrator.py
@@ -5,6 +5,7 @@ from pydantic import EmailStr, Field, SecretStr, validator
 from src.api.request_models.request_base import RequestBase
 from src.api.request_models.user import INVALID_TEXT_ERROR, VALID_TEXT
 from src.core.db.models import Administrator
+from src.core.settings import settings
 
 
 class AdministratorAuthenticateRequest(RequestBase):
@@ -32,6 +33,14 @@ class AdministratorRegistrationRequest(RequestBase):
         if not re.compile(VALID_TEXT).match(value):
             raise ValueError(INVALID_TEXT_ERROR.format("Фамилия"))
         return value.title()
+
+    @validator("password")
+    def validate_password(cls, value: SecretStr) -> SecretStr:
+        if len(value.get_secret_value()) < settings.MIN_PASSWORD_LENGTH:
+            raise ValueError(
+                "Пароль слишком короткий. Минимальная длина пароля: {}.".format(settings.MIN_PASSWORD_LENGTH)
+            )
+        return value
 
     async def parse_to_db_obj(self) -> Administrator:
         administrator = Administrator()
