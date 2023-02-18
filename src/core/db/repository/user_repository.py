9271b1d21a221e360a204662bc772a1e8d7dfe4c
Alexey-Bormotov <di-devil@yandex.ru>
2023-02-18 12:40:00 +0700

    Fixed get_user_shifts_detail statement

--- src/core/db/repository/user_repository.py
+++ src/core/db/repository/user_repository.py
@@ -35,9 +35,9 @@ class UserRepository(AbstractRepository):
                 Shift.started_at,
                 Shift.finished_at,
                 Member.numbers_lombaryers,
-                func.count(case([((Report.status == "approved"), Report.id)])).label("total_approved"),
-                func.count(case([((Report.status == "declined"), Report.id)])).label("total_declined"),
-                func.count(case([((Report.status == "waiting"), Report.id)])).label("total_skipped"),
+                func.count(case(((Report.status == "approved"), Report.id))).label("total_approved"),
+                func.count(case(((Report.status == "declined"), Report.id))).label("total_declined"),
+                func.count(case(((Report.status == "waiting"), Report.id))).label("total_skipped"),
             )
             .join(User.members, isouter=True)
             .join(Member.shift, isouter=True)
