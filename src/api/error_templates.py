75ebbd3953ea8932331f33f0017a5a08f1d8eab9
AlxShvalev <shvalevalx@gmail.com>
2023-02-22 18:56:25 +0500

    delete error_templates.py

--- src/api/error_templates.py
+++
@@ -1,7 +0,0 @@
-from src.api.response_models.error import ErrorResponse
-
-ERROR_TEMPLATE_FOR_400 = {"description": "Bad Request Response", "model": ErrorResponse}
-ERROR_TEMPLATE_FOR_401 = {"description": "Unauthorized", "model": ErrorResponse}
-ERROR_TEMPLATE_FOR_403 = {"description": "Forbidden Response", "model": ErrorResponse}
-ERROR_TEMPLATE_FOR_404 = {"description": "Not Found Response", "model": ErrorResponse}
-ERROR_TEMPLATE_FOR_422 = {"description": "Validation Error Response", "model": ErrorResponse}
