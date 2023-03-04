c6bb9ca58a233241b39fc090a1382dd83d95abc8
Geroy4ik <klevo2004@gmail.com>
2023-03-04 14:26:22 +0300

    added ROOT_PATH setting for correct working with or without proxy

--- src/application.py
+++ src/application.py
@@ -10,7 +10,7 @@ from src.core.settings import settings
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(debug=settings.DEBUG, root_path="/api")
+    app = FastAPI(debug=settings.DEBUG, root_path=settings.ROOT_PATH)
 
     origins = ["*"]
 
