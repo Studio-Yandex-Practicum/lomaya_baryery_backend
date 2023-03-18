f34335138f2c7b10e9ca72494ed4090c42565d7c
ADesBiysk <adesbiysk@yandex.ru>
2023-03-18 23:49:38 +0700

    fix: set time zone of Ufa

--- src/bot/main.py
+++ src/bot/main.py
@@ -47,23 +47,23 @@ def create_bot() -> Application:
     bot_instance.add_handler(MessageHandler(StatusUpdate.WEB_APP_DATA, web_app_data))
     bot_instance.job_queue.run_daily(
         finish_shift_automatically_job,
-        time(hour=settings.SEND_NEW_TASK_HOUR - 1, tzinfo=pytz.timezone("Europe/Moscow")),
+        time(hour=settings.SEND_NEW_TASK_HOUR - 1, tzinfo=pytz.timezone(settings.TIME_ZONE)),
     )
     bot_instance.job_queue.run_daily(
         start_shift_automatically_job,
-        time(hour=settings.SEND_NEW_TASK_HOUR, tzinfo=pytz.timezone("Europe/Moscow")),
+        time(hour=settings.SEND_NEW_TASK_HOUR, tzinfo=pytz.timezone(settings.TIME_ZONE)),
     )
     bot_instance.job_queue.run_daily(
         send_daily_task_job,
         time(
             hour=settings.SEND_NEW_TASK_HOUR,
             minute=5,  # Оставляем задержку для гарантированного старта смены
-            tzinfo=pytz.timezone("Europe/Moscow"),
+            tzinfo=pytz.timezone(settings.TIME_ZONE),
         ),
     )
     bot_instance.job_queue.run_daily(
         send_no_report_reminder_job,
-        time(hour=settings.SEND_NO_REPORT_REMINDER_HOUR, tzinfo=pytz.timezone("Europe/Moscow")),
+        time(hour=settings.SEND_NO_REPORT_REMINDER_HOUR, tzinfo=pytz.timezone(settings.TIME_ZONE)),
     )
     return bot_instance
 
