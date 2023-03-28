eb851daae16294ccba5a49aae5b9cfbd031163dc
Артемий Бутырин <artemii.butyrin@yandex.ru>
2023-03-28 23:26:01 +0300

    changed migration

--- src/core/db/models.py
+++ src/core/db/models.py
@@ -278,7 +278,7 @@ class Administrator(Base):
     status = Column(
         Enum(Status, name="administrator_status", values_callable=lambda obj: [e.value for e in obj]), nullable=False
     )
-    is_superadmin = Column(Boolean, default=False, nullable=False, server_default="FALSE")
+    is_superadmin = Column(Boolean, default=False, nullable=False)
 
     def __repr__(self) -> str:
         return f"<Administrator: {self.name} {self.surname}, role: {self.role}>"
