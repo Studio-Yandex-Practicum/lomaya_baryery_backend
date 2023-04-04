65e198388fdc3a8c6a36876f0865c247b027e307
Артемий Бутырин <artemii.butyrin@yandex.ru>
2023-04-04 17:09:06 +0300

    moved setup_logging() to create_app()

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 import uvicorn
 
 from src.application import create_app
-from src.core.utils import setup_logging
 
 app = create_app()
 
 if __name__ == '__main__':
-    setup_logging()
     uvicorn.run(app, host="0.0.0.0", port=8080)
