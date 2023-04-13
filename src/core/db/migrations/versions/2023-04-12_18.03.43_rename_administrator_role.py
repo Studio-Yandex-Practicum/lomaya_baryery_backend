12e228fe9e2accb35dfecc09a3029e54a966f1f8
Юлия Фомина <txtb@yandex.ru>
2023-04-13 16:19:22 +0300

    alembic-enums deleted. migration file changed

--- src/core/db/migrations/versions/2023-04-12_18.03.43_rename_administrator_role.py
+++ src/core/db/migrations/versions/2023-04-12_18.03.43_rename_administrator_role.py
@@ -5,35 +5,73 @@ Revises: bcda11280d4d
 Create Date: 2023-04-12 18:03:43.074512
 
 """
+import sqlalchemy as sa
 from alembic import op
-from alembic_enums import Column, EnumMigration
 
 # revision identifiers, used by Alembic.
-revision = '101e65411980'
-down_revision = 'bcda11280d4d'
+revision ="101e65411980"
+down_revision ="bcda11280d4d"
 branch_labels = None
 depends_on = None
 
 
-column = Column("administrators", "role", old_server_default=None, new_server_default=None)
+T_NAME ="administrators"
+ENUM_NAME ="administrator_role"
+TMP_NAME = f"tmp_{ENUM_NAME}"
 
+OLD_ROLES = ("administrator","psychologist",)
+NEW_ROLES = ("administrator","expert",)
+TEMP_ROLES = sorted({*OLD_ROLES, *NEW_ROLES})
 
-enum_migration = EnumMigration(
-    op=op,
-    enum_name="administrator_role",
-    old_options=["administrator", "psychologist"],
-    new_options=["administrator", "expert"],
-    columns=[column],
-)
+OLD_ENUM = sa.Enum(*OLD_ROLES, name=ENUM_NAME)
+NEW_ENUM = sa.Enum(*NEW_ROLES, name=ENUM_NAME)
+TMP_ENUM = sa.Enum(*TEMP_ROLES, name=TMP_NAME)
+
+administrators_table = sa.sql.table("administrators",
+                            sa.Column("role",
+                                      TMP_ENUM,
+                                      nullable=False)
+                            )
 
 
 def upgrade():
-    with enum_migration.upgrade_ctx():
-        enum_migration.update_value(column, "administrator", "administrator")
-        enum_migration.update_value(column, "psychologist", "expert")
+    TMP_ENUM.create(op.get_bind(), checkfirst=False)
+    op.execute(
+        f"ALTER TABLE {T_NAME} ALTER COLUMN role "
+        f"TYPE {TMP_NAME} "
+        f"USING role::text::{TMP_NAME}"
+    )
+    op.execute(
+        administrators_table.update().where(
+            administrators_table.c.role == op.inline_literal("psychologist")).values(
+            {"role": op.inline_literal("expert")})
+        )
+    OLD_ENUM.drop(op.get_bind(), checkfirst=False)
+    NEW_ENUM.create(op.get_bind(), checkfirst=False)
+    op.execute(
+        f"ALTER TABLE {T_NAME} ALTER COLUMN role "
+        f"TYPE {ENUM_NAME} "
+        f"USING role::text::{ENUM_NAME}"
+    )
+    TMP_ENUM.drop(op.get_bind(), checkfirst=False)
 
 
 def downgrade():
-    with enum_migration.downgrade_ctx():
-        enum_migration.update_value(column, "administrator", "administrator")
-        enum_migration.update_value(column, "expert", "psychologist")
+    TMP_ENUM.create(op.get_bind(), checkfirst=False)
+    op.execute(
+        f"ALTER TABLE {T_NAME} ALTER COLUMN role "
+        f"TYPE {TMP_NAME} "
+        f"USING role::text::{TMP_NAME}"
+    )
+    op.execute(
+        administrators_table.update().where(
+            administrators_table.c.role == op.inline_literal("expert")).values(
+            {"role": op.inline_literal("psychologist")}))
+    NEW_ENUM.drop(op.get_bind(), checkfirst=False)
+    OLD_ENUM.create(op.get_bind(), checkfirst=False)
+    op.execute(
+        f"ALTER TABLE {T_NAME} ALTER COLUMN role "
+        f"TYPE {ENUM_NAME} "
+        f"USING role::text::{ENUM_NAME}"
+    )
+    TMP_ENUM.drop(op.get_bind(), checkfirst=False)
