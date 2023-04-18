ee627bf877be6d2d15fca95504f8012f5dad3964
Илья Ватютов <vatut007@ya.ru>
2023-04-18 17:05:42 +0300

    fix remarks

--- src/excel_generator/task_builder.py
+++ src/excel_generator/task_builder.py
@@ -1,21 +1,10 @@
-
-class classproperty(object):
-    def __init__(self, fget):
-        self.fget = fget
-
-    def __get__(self, owner_self, owner_cls):
-        return self.fget(owner_cls)
-
-
 class BaseAnalyticReportSettings:
     def __init__(self,
                  sheet_name: str,
                  header_data: tuple[tuple[str]],
-                 footer_data: tuple[str],
                  row_count: int):
         self.sheet_name = sheet_name
         self.header_data = header_data
-        self.footer_data = footer_data
         self.row_coun = row_count
 
 
@@ -24,11 +13,12 @@ class TaskAnalyticReportSettings(BaseAnalyticReportSettings):
 
     sheet_name: str = "Задачи"
     header_data: tuple[str] = (
-        ("Задача", "Кол-во принятых отчётов", "Кол-во отклонённых отчётов", "Кол-во не предоставленных отчётов"),
+        "Задача", "Кол-во принятых отчётов", "Кол-во отклонённых отчётов", "Кол-во не предоставленных отчётов"
     )
     row_count: int = 0
 
-    @classproperty
+    @classmethod
+    @property
     def footer_data(self):
         result = (
             "ИТОГО:",
