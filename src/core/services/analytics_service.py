ac1c4791571e203f5d6b10758d0802bdc78baae8
vatut007 <vatut007@ya.ru>
2023-04-22 13:42:17 +0300

    fix remarks

--- src/core/services/analytics_service.py
+++ src/core/services/analytics_service.py
@@ -12,7 +12,9 @@ class AnalyticsService:
     """Сервис для получения отчётов."""
 
     def __init__(
-        self, task_repository: TaskRepository = Depends(), task_report_builder: AnalyticReportBuilder = Depends()
+        self,
+        task_repository: TaskRepository = Depends(),
+        task_report_builder: AnalyticReportBuilder = Depends(),
     ) -> None:
         self.__task_report_builder = task_report_builder
         self.__task_repository = task_repository
@@ -21,7 +23,9 @@ class AnalyticsService:
         """Генерация отчёта с заданиями."""
         tasks_statistic = await self.__task_repository.get_tasks_statistics_report()
         await self.__task_report_builder.generate_report(
-            tasks_statistic, workbook=workbook, analytic_task_report_full=TaskAnalyticReportSettings
+            tasks_statistic,
+            workbook=workbook,
+            analytic_task_report_full=TaskAnalyticReportSettings,
         )
 
     async def generate_full_report(self) -> BytesIO:
@@ -29,12 +33,10 @@ class AnalyticsService:
         workbook = self.__task_report_builder.create_workbook()
         await self.__generate_task_report(workbook)
         await self.__generate_task_report(workbook)
-        stream = await self.__task_report_builder.get_report_response(workbook)
-        return stream
+        return await self.__task_report_builder.get_report_response(workbook)
 
     async def generate_task_report(self) -> BytesIO:
         """Генерация отчёта с заданиями."""
         workbook = self.__task_report_builder.create_workbook()
         await self.__generate_task_report(workbook)
-        stream = await self.__task_report_builder.get_report_response(workbook)
-        return stream
+        return await self.__task_report_builder.get_report_response(workbook)
