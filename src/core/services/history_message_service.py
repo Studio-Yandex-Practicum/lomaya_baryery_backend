984714b80f1b86cd7bb5066f60531a390579872a
Никита Ефремов <nikita_efremov_1995.09.14@mail.ru>
2023-04-24 14:07:01 +0700

    create class MessageHistoryService

---
+++ src/core/services/history_message_service.py
@@ -0,0 +1,21 @@
+from fastapi import Depends
+
+from src.core.db.models import MessageHistory
+from src.core.db.repository import MessageHistoryRepository, ShiftRepository
+
+
+class MessageHistoryService:
+    def __init__(
+        self,
+        history_message_repository: MessageHistoryRepository = Depends(),
+        shift_repository: ShiftRepository = Depends(),
+    ) -> None:
+        self.__history_message_repository = history_message_repository
+        self.__shift_repository = shift_repository
+
+    async def create_history_message(self, user_id, chat_id, message, status) -> None:
+        shift = self.__shift_repository.get_started_shift_id()
+        history_message = MessageHistory(
+            user_id=user_id, chat_id=chat_id, message=message, status=status, shift_id=shift
+        )
+        await self.__history_message_repository.create(instance=history_message)
